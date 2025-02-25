17d401be8d9c82b0792257440235610d22eb3107
ThinhLam <thinhlam240499@gmail.com>
2025-02-25 20:10:26 +0700

    remove nodemon

--- src/config/database.js
+++ src/config/database.js
@@ -10,8 +10,6 @@ const dbState = [
 
 const connection = async () => {
   const options = {
-    user: process.env.DB_USER,
-    pass: process.env.DB_PASSWORD,
     dbName: process.env.DB_NAME,
   };
 
